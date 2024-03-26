06261c4a52bf50d4a7833b079a44957525acf44f
Jakub Plecháč <plechac.k@gmail.com>
2024-03-26 14:35:35 +0100

    Inicializace projektu

---
+++ PvaMaturit/PvaMaturit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PvaMaturit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
